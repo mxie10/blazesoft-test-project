67babd496422eb74ba0c6784b766336c08947b3a
mxie10 <xieyz13@gmail.com>
2024-06-07 11:16:59 -0400

    remove testing code

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import Container from '@/app/components/Container';
 import Listings from '@/app/components/Listings';
-import CheckBox from './components/checkBox';
 import { useRef, useState } from 'react';
 
 export default function Dashboard() {
@@ -29,40 +28,9 @@ export default function Dashboard() {
 
   return (
     <Container>
-      {/* <div className='w-full lg:w-4/5'>
+      <div className='w-full lg:w-4/5'>
         <Listings />
-      </div> */}
-
-      {/* <div>
-        <p>countRef: {countRef.current}</p>
-        <button
-          onClick={() => {countRef.current += 1; console.log(countRef.current)}}
-        >
-          Ref button
-        </button>
-      </div> */}
-
-      <div className='flex flex-row gap-2'>
-        <label>select all</label>
-        <input 
-          type='checkbox' 
-          checked={(selectAllStatus())} 
-          onChange={selectAll}
-        />
       </div>
-      {checkStatus.map((status, index) => {
-        return (
-          <div className='flex flex-row gap-2'>
-            <label>checkbox {index + 1}</label>
-            <input 
-              type='checkbox' 
-              checked={status} 
-              onChange={() => handleIndividual()}
-            />
-          </div>
-        )
-      })}
-
     </Container>
   )
 }
